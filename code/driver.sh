f13afc762561ceb3083c5227ac1dd3f878316916
gkiar <gkiar07@gmail.com>
2020-11-19 12:45:43 -0500

    updated driver for new wrapper script

--- code/driver.sh
+++ code/driver.sh
@@ -20,26 +20,25 @@ mkdir -p ${resd} ${jobp}
 if [[ ${DSET} == "D100" ]]
 then
   exps="mca"
-  targets="bmi age cholesterol sex rel_vo2max"
-  timest="06:00:00"
+  targets="bmi age sex rel_vo2max"
+  timest="02:00:00"
 else
   exps="mca session subsample"
   targets="bmi age sex rel_vo2max"
   timest="00:30:00"
 fi
 
-nmca="20 15 10 5 2"
+nmca="20 18 16 14 12 10 7 5 2"
 aggs="ref meta mega mean median consensus"
-classifs="SVM LR RF"
 
 for t in ${targets}
 do
-  for c in ${classifs}
+  for n in ${nmca}
   do
     for e in ${exps}
     do
-      logf="${jobp}log_${DSET}_${t}_${c}_${e}.txt"
-      cat << TMP > ${jobp}exec_${t}_${c}_${e}.sh
+      logf="${jobp}log_${DSET}_${t}_${n}_${e}.txt"
+      cat << TMP > ${jobp}exec_${t}_${n}_${e}.sh
 #!/bin/bash
 #SBATCH --time ${timest}
 #SBATCH --mem 16G
@@ -61,19 +60,16 @@ for a in \${agg_iter}
 do
   if [[ \${exp} == "mca" ]]
   then
-    for n in ${nmca}
-    do
-      echo $e $t \$a $c \$n &>> ${logf}
-      (time python model_wrapper.py ${resd} ${dset} ${e} ${t} \${a} ${c} --n_mca \${n} --verbose) &>>${logf}
-    done
+    echo $e $t \$a $n &>> ${logf}
+    (time python model_wrapper.py ${resd} ${dset} ${e} ${t} \${a} --n_mca ${n} --verbose) &>>${logf}
   else
-    echo $e $t \$a $c &>> ${logf}
-    (time python model_wrapper.py ${resd} ${dset} ${e} ${t} \${a} ${c} --verbose) &>>${logf}
+    echo $e $t \$a &>> ${logf}
+    (time python model_wrapper.py ${resd} ${dset} ${e} ${t} \${a} --verbose) &>>${logf}
   fi
 done
 TMP
-      chmod +x ${jobp}exec_${t}_${c}_${e}.sh
-      sbatch ${jobp}exec_${t}_${c}_${e}.sh
+      chmod +x ${jobp}exec_${t}_${n}_${e}.sh
+      # sbatch ${jobp}exec_${t}_${n}_${e}.sh
     done
   done
 done
