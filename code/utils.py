908c83b0a133a7a10197ec28b3d068eb73317dc1
gkiar <gkiar07@gmail.com>
2020-12-09 19:02:53 -0500

    added significant digit computation and truncation code

--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from netneurotools.networks import struct_consensus
+import warnings
 import numpy as np
 
 
@@ -23,12 +24,62 @@ def refSample(gs, index=0):
     return gs[:,:,index]
 
 
+def refTrunc(gs, index=0):
+    # Compute edge-wise significant digits
+    digits = np.ceil(sigdig(gs, base=10, axis=2)).astype(int)
+
+    # Grab reference graph and non-zero locations
+    ref = refSample(gs, index=index)
+    loc = np.where(ref != 0)
+
+    def trunc(value, prec):
+        fstr = ("{0:1." + str(prec-1) + "e}").format(value)
+        return np.float64(fstr).astype(value.dtype)
+
+    for l0, l1 in zip(loc[0], loc[1]):
+        ref[l0, l1] = trunc(ref[l0, l1], digits[l0, l1])
+
+    return ref
+
+
 def unstratifiedSample(gs, verbose=False):
     # Pick random value in a list and return the corresponding graph
     idx = np.random.choice(gs.shape[-1], size=1)[0]
     if verbose:
         print(idx)
     return gs[:,:,idx]
+
+
+def sigdig(array, base=2, axis=None):
+    try:
+        # If we have a float, this is our value of epsilon
+        eps = np.finfo(array.dtype).eps
+    except ValueError:
+        # If it's an int, we want to convert it to a float of the same number of
+        # bits to get our estimate of epsilon
+        a2_dtype = "np.float{0}".format(array.dtype.itemsize*8)
+        a2 = array.astype(eval(a2_dtype))
+        warnings.warn("Converting array from {} to {}".format(array.dtype,
+                                                              a2.dtype),
+                      Warning)
+        # Re-call the function with the float version
+        return sigdig(a2, base=base, axis=axis)
+
+    # Compute the standard deviation and handle special case 1:
+    #   - if no variance, maximum significance
+    sd = np.std(array, axis=axis)
+    if np.all(sd == 0):
+        return -np.log(eps)/np.log(base)
+
+    # Compute the mean and handle special case 2:
+    #   - if mean of 0, no significance.
+    #   - N.B. this is the incorrect formula for zero-centered data
+    mn = np.mean(array, axis=axis)
+    if np.all(mn == 0):
+        return 0
+
+    # Otherwise, compute the number of significant digits using Parker, 1997
+    return -np.log(sd / mn + eps)/np.log(base)
 
 
 def qcAggregate(gs, agg, g_ind=0, log=True):
